94a95c84588eed472a4b123b35a5197771e90525
jaredandersgilbert@gmail.com <jaredandersgilbert@gmail.com>
2017-01-24 11:51:20 -0500

    comments legacy spotify callback

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -8,24 +8,24 @@ class PagesController < ApplicationController
     end
   end
 
-  def spotify_callback
-    auth_code = params[:code]
-    auth_code ||= "AQCfcvwJk3H4MCJH4YDk4urIoc3srg5Q92XLr0vpSOu8fkPyRfxpAL7o9cyrBtESsypPSmAzT5iznNPmR1E9Ke9Dat59GQphd5WVHOz19N8zgs1YemUyGgvj8UP_ENwTEde6U8k3QopfOp2Cv7xxZtZBDMPTLiGzFz7zgQ1Aie4a0IlGFXe4raYToBA9as6RktKfE417G7nHEZLYgVMDov_iPM_JKYcLvu6xOAQIOGpIP9dXjb4uHFB6gaT161lMZRnxHY6einl7JH8-"
-
-    http = Net::HTTP.new("accounts.spotify.com")
-    http.ssl_version = :SSLv3
-    http.verify_mode = OpenSSL::SSL::VERIFY_NONE
-
-    request = Net::HTTP::Post.new("/api/token")
-    request.set_form_data({
-      "grant_type" => "authorization_code",
-      "code" => auth_code,
-      "redirect_uri" => @redirect_uri
-      })
-    @response = http.request(request)
-  end
-
-  def set_redirect_uri
-    @redirect_uri = CGI.escape("#{ENV['SPOTIFY_CALLBACK']}/spotify-callback")
-  end
+  # def spotify_callback
+  #   auth_code = params[:code]
+  #   auth_code ||= "AQCfcvwJk3H4MCJH4YDk4urIoc3srg5Q92XLr0vpSOu8fkPyRfxpAL7o9cyrBtESsypPSmAzT5iznNPmR1E9Ke9Dat59GQphd5WVHOz19N8zgs1YemUyGgvj8UP_ENwTEde6U8k3QopfOp2Cv7xxZtZBDMPTLiGzFz7zgQ1Aie4a0IlGFXe4raYToBA9as6RktKfE417G7nHEZLYgVMDov_iPM_JKYcLvu6xOAQIOGpIP9dXjb4uHFB6gaT161lMZRnxHY6einl7JH8-"
+  #
+  #   http = Net::HTTP.new("accounts.spotify.com")
+  #   http.ssl_version = :SSLv3
+  #   http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+  #
+  #   request = Net::HTTP::Post.new("/api/token")
+  #   request.set_form_data({
+  #     "grant_type" => "authorization_code",
+  #     "code" => auth_code,
+  #     "redirect_uri" => @redirect_uri
+  #     })
+  #   @response = http.request(request)
+  # end
+  #
+  # def set_redirect_uri
+  #   @redirect_uri = CGI.escape("#{ENV['SPOTIFY_CALLBACK']}/spotify-callback")
+  # end
 end
